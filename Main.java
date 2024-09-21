6eba5bba958060e8b681a8a5abdb630847fabb2a
YingZhi0818 <144502969+YingZhi0818@users.noreply.github.com>
2024-09-21 16:00:51 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,33 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-import java.io.File;
-import java.io.IOException;
 
 public class Main {
-
+	
+	
+    static List<Staff> staffList = new ArrayList<>();
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
         Product product = new Product();
+        Payment payment = new Payment();
+        Stock stock = new Stock();
         
-        // Ensure that the products file exists
-        try {
-            File file = new File("products.txt");
-            if (!file.exists()) {
-                file.createNewFile();
-                System.out.println("File created: " + file.getName());
-            } 
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
+        boolean exit = false;
 
-        // Main menu loop
+       
         while (true) {
-            System.out.println("\nMain Menu:");
-            System.out.println("1. Supplier Management");
-            System.out.println("2. Product Management");
-            System.out.println("3. Order Management");
-            System.out.println("4. Exit");
+            System.out.println("\n|        Main Menu        |");
+            System.out.println("===========================");
+            System.out.println("|1. Supplier Management   |");
+            System.out.println("|2. Product Management    |");
+            System.out.println("|3. Order Management      |");
+            System.out.println("|4. Transaction Management|");
+            System.out.println("|5. Staff Management      |");
+            System.out.println("|6. Display Stock         |");
+            System.out.println("|7. Exit                  |");
+            System.out.println("===========================");
             System.out.print("Enter your choice: ");
             
             if (scanner.hasNextInt()) {
@@ -42,14 +41,25 @@ public class Main {
                         product.productMenu(); 
                         break;
                     case 3:
-                        Order.userInterface(scanner); 
-                        break;    
+                        Order.userInterface(scanner	); 
+                        break;
                     case 4:
+                        payment.paymentMenu(); 
+                        break;
+                    case 5:
+                        Staff.staffMenu(scanner, staffList);
+                        break;
+                    case 6:
+                        stock.displayStock();
+                        System.out.println("Press Enter to return to the main menu...");
+    					scanner.nextLine(); 
+                        break;        
+                    case 7:
                         System.out.println("Exiting...");
                         scanner.close();
                         return; 
                     default:
-                        System.out.println("Invalid choice. Please enter a number between 1 and 4.");
+                        System.out.println("Invalid choice. Please enter a number between 1 and 6.");
                 }
             } else {
                 System.out.println("Invalid input. Please enter a number.");
