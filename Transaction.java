582daee06680f6bb72bdccadf23a5e82588111ac
Rensint <kaihen0623@gmail.com>
2024-09-22 17:12:50 +0800

    Update Transaction.java

--- Transaction.java
+++ Transaction.java
@@ -8,6 +8,9 @@ public class Transaction {
     
     private static final String TRANSACTION_FILE = "transactions.txt";
     
+    public Transaction(){
+    }
+    
     public Transaction(double amount) {
         this.transactionID = generateTransactionID();
         this.transactionDate = new Date(); 
@@ -25,53 +28,57 @@ public class Transaction {
     public double getAmount() {
         return amount;
     }
+   
+  	public String getTransactionFile(){
+  		return TRANSACTION_FILE;
+  	}
 
 	public String generateTransactionID() {
-    int highestID = 0; 
-
-    try (BufferedReader reader = new BufferedReader(new FileReader(TRANSACTION_FILE))) {
-        String line;
-        while ((line = reader.readLine()) != null) {
-            
-            if (line.startsWith("Transaction ID:")) {
-                
-                String[] transactionDetails = line.split(",");
-                for (String detail : transactionDetails) {
-                    if (detail.trim().startsWith("Transaction ID:")) {
-                        String id = detail.split(":")[1].trim();
-                       
-                        if (id.length() > 1 && id.startsWith("T")) {
-                            try {
-                                int num = Integer.parseInt(id.substring(1)); // Remove 'T'
-                                if (num > highestID) {
-                                    highestID = num;
-                                }
-                            } catch (NumberFormatException e) {
-                                
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-
-    highestID++; 
-
-    
-    return String.format("T%03d", highestID);
-}
-
-
+	    int highestID = 0; 
+	
+	    try (BufferedReader reader = new BufferedReader(new FileReader(TRANSACTION_FILE))) {
+	        String line;
+	        while ((line = reader.readLine()) != null) {
+	            
+	            if (line.startsWith("Transaction ID:")) {
+	                
+	                String[] transactionDetails = line.split(",");
+	                for (String detail : transactionDetails) {
+	                    if (detail.trim().startsWith("Transaction ID:")) {
+	                        String id = detail.split(":")[1].trim();
+	                       
+	                        if (id.length() > 1 && id.startsWith("T")) {
+	                            try {
+	                                int num = Integer.parseInt(id.substring(1)); // Remove 'T'
+	                                if (num > highestID) {
+	                                    highestID = num;
+	                                }
+	                            } catch (NumberFormatException e) {
+	                                
+	                            }
+	                        }
+	                    }
+	                }
+	            }
+	        }
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	
+	    highestID++; 
+	
+	    
+	    return String.format("T%03d", highestID);
+	}
 
 
-    public void saveTransaction() {
+
+
+    public void saveTransaction(String transactionID, Date transactionDate, double amount) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(TRANSACTION_FILE, true))) {
-            writer.write("Transaction ID: " + getTransactionID() + ", ");
-            writer.write("Transaction Date: " + getTransactionDate() + ", ");
-            writer.write("Amount: RM" + String.format("%.2f", amount) + "\n");
+            writer.write("Transaction ID: " + transactionID + ", ");
+            writer.write("Transaction Date: " + transactionDate + ", ");
+            writer.write("Amount: RM" + String.format("%.2f", amount) + ", ");
         } catch (IOException e) {
             e.printStackTrace();
         }
